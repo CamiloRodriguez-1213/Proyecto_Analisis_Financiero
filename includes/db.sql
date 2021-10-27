2e2f871a0bb1368c1b96b7a6aa931906e7b57bbe
Camilo Rodriguez <andrescamilocr482@gmail.com>
2021-10-27 12:58:17 -0500

    Base de datos

---
+++ includes/db.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para proyecto
+CREATE DATABASE IF NOT EXISTS `proyecto` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `proyecto`;
+
+-- Volcando estructura para tabla proyecto.inventario
+CREATE TABLE IF NOT EXISTS `inventario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_producto` varchar(50) NOT NULL DEFAULT '0',
+  `categoria` varchar(50) DEFAULT NULL,
+  `cantidad` int(11) NOT NULL DEFAULT '0',
+  `precio_unitario` int(11) NOT NULL DEFAULT '0',
+  `precio_total` int(11) NOT NULL DEFAULT '0',
+  `fecha` date DEFAULT NULL,
+  `imagen_producto` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla proyecto.inventario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+
+-- Volcando estructura para tabla proyecto.personas
+CREATE TABLE IF NOT EXISTS `personas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) DEFAULT NULL,
+  `acciones` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla proyecto.personas: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
+INSERT INTO `personas` (`id`, `nombre`, `email`, `acciones`) VALUES
+	(1, 'alex', 'alexgmail.com', 'asdfasdf'),
+	(2, 'asd', 'asdf', ' adsdf'),
+	(3, 'AndrÃ©s Camilo', 'andrescamilocr482@gmail.com', ' perico'),
+	(4, 'mouse', 'gamer', ' 60000');
+/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
